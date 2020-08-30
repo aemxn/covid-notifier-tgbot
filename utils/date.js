62f40a558d249c26167a42e7ab5030557c873238
AIman Baharum <aiman.baharum@outlook.com>
2020-08-31 01:31:58 +0800

    formate date function

--- utils/date.js
+++ utils/date.js
@@ -1,16 +1,31 @@
 const moment = require('moment')
 
+const timeFormat = 'DD/MM/YYYY, HH:mm:ss';
+const dateFormat = 'DD/MM/YYYY'
+
 function getNow() {
     moment().locale('ms-my');
-    const nowText = moment().format('DD/MM/YYYY, HH:mm:ss');
+    const nowText = moment().format(timeFormat);
     return nowText;
 }
 
 function getDuration(timestamp) {
     moment().locale('ms-my');
     //20200829,240103
-    const duration = moment(timestamp, 'DD/MM/YYYY, HH:mm:ss').fromNow();
+    const duration = moment(timestamp, timeFormat).fromNow();
     return duration;
 }
 
-module.exports = { getNow, getDuration }
+function formatDate(timestamp, time) {
+    moment().locale('ms-my');
+    let format = '';
+    if (time) {
+        format = timeFormat;
+    } else {
+        format = dateFormat;
+    }
+    const noTime = moment(timestamp, timeFormat).format(format);
+    return noTime;
+}
+
+module.exports = { getNow, getDuration, formatDate }
